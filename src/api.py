7f5952ad0a07582c7ba196c57e3eefef3856cc24
Rishant78 <rikushwaha78.rk@gmail.com>
2026-09-26 02:50:47 +0530

    Fix CORS configuration to allow all origins

--- src/api.py
+++ src/api.py
@@ -13,8 +13,8 @@ app=FastAPI(title="PII Redaction API", version="1.0.0")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[os.environ.get("FRONTEND_URL", "http://localhost:3000")],
-    allow_credentials=True,
+    allow_origins=["*"],
+    allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
 )
